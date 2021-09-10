7677056343aa2ad373ec75994133ca8a9ea9e818
André Rainho <arainho.it@gmail.com>
2021-09-10 09:54:05 +0100

    Create linux_connection_share.sh

---
+++ scripts/linux_connection_share.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+Linux - Internet connection share
+
+# Activate IP forwarding
+sysctl -w net.ipv4.ip_forward=1
+sysctl -p
+
+# NAT configuration
+iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -I FORWARD  -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -I POSTROUTING -o ppp0 -j MASQUERADE

3f5cc87d557c421feddb0531ebee8d7828ad8364
André Rainho <arainho.it@gmail.com>
2021-09-10 09:56:58 +0100

    Update linux_connection_share.sh

--- scripts/linux_connection_share.sh
+++ scripts/linux_connection_share.sh
@@ -3,11 +3,13 @@
 Linux - Internet connection share
 
 # Activate IP forwarding
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo  sysctl -p
+sysctl -w net.ipv4.ip_forward=1
+sysctl -p
 
 # NAT configuration
-sudo iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -I FORWARD  -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -t nat -I POSTROUTING -o usb0 -j MASQUERADE
-sudo iptables -nvL
+iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -I FORWARD  -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -nvL
+
+iptables -t nat -I POSTROUTING -o usb0 -j MASQUERADE
+iptables -nvL -t nat

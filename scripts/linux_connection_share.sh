f1955914e7fe095416500e926ec7060204c45bab
André Rainho <arainho.it@gmail.com>
2021-09-10 10:10:03 +0100

    fix script

--- scripts/linux_connection_share.sh
+++ scripts/linux_connection_share.sh
@@ -4,11 +4,18 @@ Linux - Internet connection share
 
 # Activate IP forwarding
 sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.default.forwarding=1
+sysctl -w net.ipv6.conf.all.forwarding=1
 
 # NAT configuration
-iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -I FORWARD  -m state --state RELATED,ESTABLISHED -j ACCEPT
+# wlan0 - interface connected to the internet on your laptop or workstation
+# usb0 - interface connected to the USB Armory
+
+iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+iptables -nvL -t nat
+
+iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i usb0 -o wlan0 -j ACCEPT
 iptables -nvL
 
-iptables -t nat -I POSTROUTING -o usb0 -j MASQUERADE
-iptables -nvL -t nat
+
